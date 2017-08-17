9e65b0393427f25ced5164751c6f007b27163413
Asbjørn A. Fellinghaug <asbjorn@fellinghaug.com>
2017-08-17 15:54:08 +0200

    Added logic to auto create database + tables if not found

--- ublox_mon/db.py
+++ ublox_mon/db.py
@@ -1,11 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy_utils.functions import database_exists, create_database
 
 from .models import Base, JamTimeseries
 
 # Create an engine that stores data in the local directory's
 # sqlalchemy_example.db file.
-engine = create_engine('sqlite:///data.db', convert_unicode=True)
+
+database_path = "sqlite:///data.db"
+created = False
+if not database_exists(database_path):
+    created = True
+    create_database(database_path)
+
+engine = create_engine(database_path, convert_unicode=True)
 session = scoped_session(sessionmaker(autocommit=False,
                                       autoflush=False,
                                       bind=engine))
@@ -14,3 +22,8 @@ Base.query = session.query_property()
 
 def init_db():
     Base.metadata.create_all(bind=engine)
+
+
+if created:
+    print("Just created new db")
+    init_db()
